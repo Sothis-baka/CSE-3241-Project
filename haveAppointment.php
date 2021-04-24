f2b9566607d774a8cf5b85db7d3e1cdb9a0943a8
Cyrus Li <70495046+Sothis-baka@users.noreply.github.com>
2021-04-24 15:12:18 -0400

    Update haveAppointment.php

--- haveAppointment.php
+++ haveAppointment.php
@@ -65,7 +65,7 @@
 	_HTML_;
     mysqli_close($conn);
     ?>
-    <a href="adminIndex.php" class="btn btn-primary form-control">back</a>
+    <a href="adminIndex.php" class="btn btn-primary form-control">Back</a>
 </div>
 </body>
 </html>
